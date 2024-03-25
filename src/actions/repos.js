e89e4e0e4eb8ab291d4fbbe4dda042878b107858
freetimezp <kraftukrnet@ukr.net>
2024-03-25 17:40:27 +0200

    06. js-react-01.Searching Github repositories by name.

--- src/actions/repos.js
+++ src/actions/repos.js
@@ -1,8 +1,10 @@
 import axios from "axios";
-import { setRepos } from '../reducers/reposReducer';
+import { setIsFetching, setRepos } from '../reducers/reposReducer';
 
 export const getRepositories = (searchQuery = "stars:%3E1") => {
     return async (dispatch) => {
+        dispatch(setIsFetching(true));
+
         const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`);
         dispatch(setRepos(response.data));
     }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './main.less';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRepositories } from '../../actions/repos';
@@ -8,6 +8,13 @@ const Main = () => {
     const dispatch = useDispatch();
 
     const repos = useSelector(state => state.repos.items);
+    const isFetching = useSelector(state => state.repos.isFetching);
+
+    const [searchValue, setSearchValue] = useState('');
+
+    const searchHandler = () => {
+        searchValue !== '' ? dispatch(getRepositories(searchValue)) : dispatch(getRepositories());
+    }
 
     useEffect(() => {
         dispatch(getRepositories());
@@ -15,9 +22,25 @@ const Main = () => {
 
     return (
         <div>
-            {repos && repos.length > 0 && repos.map((repo, i) => (
-                <Repo key={i} repo={repo} />
-            ))}
+            <div className='search'>
+                <input
+                    type="text"
+                    placeholder='Search repository by name'
+                    className='search-input'
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                />
+                <button className='search-btn' onClick={() => searchHandler()}>
+                    Search
+                </button>
+            </div>
+            {isFetching === false ? (
+                repos && repos.length > 0 && repos.map((repo, i) => (
+                    <Repo key={i} repo={repo} />
+                ))
+            ) : (
+                <div className='fetching'></div>
+            )}
         </div>
     );
 }
